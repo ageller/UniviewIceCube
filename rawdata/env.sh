8df2e280d9aa51d8225f769b509adf0d6bc286a9
Aaron Geller <ageller@williamson.local>
2018-09-17 11:53:21 -0500

    converted IceCube geometry to csv

--- rawdata/env.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-export x="C:\Users\Aaron\\ Geller\SCISS\modulesbuilder\UniviewIceCube\testing\code\odf_tarball"
-export PYTHONPATH=${PYTHONPATH}":${x}\python:${x}\tests:${x}\event_viewer"
