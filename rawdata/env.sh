06dcf9fbcea7afb00f3075d5bbcae2e2cb89bd90
Aaron Geller <a-geller@northwestern.edu>
2018-09-17 11:39:28 -0500

    small update from Windows side

---
+++ rawdata/env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+export x="C:\Users\Aaron\\ Geller\SCISS\modulesbuilder\UniviewIceCube\testing\code\odf_tarball"
+export PYTHONPATH=${PYTHONPATH}":${x}\python:${x}\tests:${x}\event_viewer"
